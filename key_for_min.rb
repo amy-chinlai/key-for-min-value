ec87fdd7688857846c56f8316a1113f36514fde4
amy-chinlai <amy@actionnetwork.org>
2021-06-29 14:58:40 -0400

    commit tree 8c468c8d26c6c88a5bfa8dc55261652bef3d44c4

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,23 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-    min = ""
-    name_hash.values.each_with_index do |v, index|
-        if v < values[index + 1]
-            return v
+    # min = ""
+    # name_hash.values.each_with_index do |v, index|
+    #     if v < values[index + 1]
+    #         return v
+    #     end
+    # end
+    # return name_hash.index()
+    # binding.pry
+    #ikea = {:chair => 25, :table => 85, :mattress => 450}
+    lowest_value = nil
+    lowest_key = nil
+    name_hash.each do |key, value|
+        if lowest_value == nil || value < lowest_value
+            lowest_value = value
+            lowest_key = key
         end
+        
     end
-    return name_hash.index()
-    binding.pry
+    return lowest_key
 end
